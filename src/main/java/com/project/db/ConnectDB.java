9dede7fc1b70c02148fb1b6ee923652b2f3af961
jason <363317018@qq.com>
2017-05-05 09:45:29 +0800

    清空无用文件

--- src/main/java/com/project/db/ConnectDB.java
+++ src/main/java/com/project/db/ConnectDB.java
@@ -1,6 +1,5 @@
-package com.jasonpiao.db;
+package com.project.db;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Component;
