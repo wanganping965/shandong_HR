3e627742c9a8f4196a343dbf0f8344618437acc8
ZhentangHe <727633865@qq.com>
2017-05-07 23:39:32 +0800

    He Zhentang

---
+++ src/main/java/com/project/db/controller/ProvinceController.java
@@ -0,0 +1,15 @@
+package com.project.db.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by He Zhentang on 2017/5/7.
+ */
+@Controller
+//拦截http://localhost/province
+@RequestMapping("/province")
+
+public class ProvinceController {
+
+}
